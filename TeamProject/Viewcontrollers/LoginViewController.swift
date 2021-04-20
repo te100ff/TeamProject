1a7e4f9e38ab6ed66fb9b3c59d0f7cb7067d7bb6
Stanislav <te100ff@inbox.ru>
2021-04-20 12:29:39 +0300

    Ok button round form

--- TeamProject/Viewcontrollers/LoginViewController.swift
+++ TeamProject/Viewcontrollers/LoginViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    @IBOutlet weak var okButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        okButton.layer.cornerRadius = okButton.frame.height/2
     }
 
 
