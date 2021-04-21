61c2978ecec54b5e5001eb50ca5bea28a6e0d539
Stanislav <te100ff@inbox.ru>
2021-04-21 23:24:02 +0300

    LoginVC structure pre complete

--- TeamProject/Viewcontrollers/LoginViewController.swift
+++ TeamProject/Viewcontrollers/LoginViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    // MARK: - IB Outlets
     @IBOutlet weak var okButton: UIButton!
     
     @IBOutlet weak var nameTF: UITextField!
@@ -16,69 +17,59 @@ class LoginViewController: UIViewController {
     
     @IBOutlet var labels: [UILabel]!
     
+    // MARK: - Private properties
+    private let datePicker = UIDatePicker()
     
-    let datePicker = UIDatePicker()
-    
-    
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
         okButton.layer.cornerRadius = okButton.frame.height/2
         okButton.alpha = 0.3
-        //view.backgroundColor = UIColor(patternImage: UIImage(named: "stars")!)
         assignbackground()
         labelsSetup()
     }
+    
     //подготовка сегвея
     /*
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let greetingVC = segue.destination as! UITabBarController
-        
-        guard let viewControlers = greetingVC.viewControllers else { return }
-        
-        for viewController in viewControlers {
-            if let welcomeVC = viewController as? WelcomeViewController {
-                //
-            } else if let aboutVC = viewController as? AboutViewController {
-                //
-            } else if let zodiacVC = viewController as? ZodiacViewController {
-                //
-            }  else if let navigationVC = viewController as? UINavigationController {
-                let friendsVC = navigationVC.topViewController as! BioViewController
-                //
-            }
-        }
-    }
-    */
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     let greetingVC = segue.destination as! UITabBarController
+     
+     guard let viewControlers = greetingVC.viewControllers else { return }
+     
+     for viewController in viewControlers {
+     if let welcomeVC = viewController as? WelcomeViewController {
+     //
+     } else if let aboutVC = viewController as? AboutViewController {
+     //
+     } else if let zodiacVC = viewController as? ZodiacViewController {
+     //
+     }  else if let navigationVC = viewController as? UINavigationController {
+     let friendsVC = navigationVC.topViewController as! BioViewController
+     //
+     }
+     }
+     }
+     */
     
+    // MARK: - IB Actions
     @IBAction func okButtonPressed() {
         okPressed(textFields: nameTF, surnameTF, birthdayTF)
     }
     
-    func assignbackground(){
-            let background = UIImage(named: "stars")
-
-            var imageView : UIImageView!
-            imageView = UIImageView(frame: view.bounds)
-        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-            imageView.clipsToBounds = true
-            imageView.image = background
-            imageView.center = view.center
-            view.addSubview(imageView)
-            self.view.sendSubviewToBack(imageView)
-        }
-    
-    
-    func labelsSetup() {
-        for label in labels {
-            label.layer.shadowColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            label.layer.shadowOpacity = 1
-            label.layer.shadowRadius = 15
-            label.layer.shadowOffset = .zero
+    // MARK: - Private methods (button pressed)
+    private func okPressed(textFields: UITextField... ) {
+        textFields.forEach { textField in
+            guard let text = textField.text, !text.isEmpty else {
+                showAlert(title: "Внимание", message: "Заполните все поля")
+                return
+            }
         }
     }
     
+    
 }
 
+// MARK: - Textfields and keyboards setup
 extension LoginViewController: UITextFieldDelegate  {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
@@ -130,7 +121,7 @@ extension LoginViewController: UITextFieldDelegate  {
         textField.layer.shadowOpacity = 0
         okButtonChange(textFields: nameTF, surnameTF, birthdayTF)
     }
-
+    
     
     @objc func doneDatePressed() {
         let formater = DateFormatter()
@@ -143,39 +134,55 @@ extension LoginViewController: UITextFieldDelegate  {
         view.endEditing(true)
     }
     
-    private func textFieldSetup(textField: UITextField) {
-        textField.layer.shadowOpacity = 1
-        textField.layer.shadowOffset = .zero
-       textField.layer.shadowRadius = 10
-        textField.layer.shadowColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
-    }
-    
-    private func okPressed(textFields: UITextField... ) {
-        textFields.forEach { textField in
-            guard let text = textField.text, !text.isEmpty else {
-                showAlert(title: "Внимание", message: "Заполните все поля", textField: textField)
-                return
-            }
-            
-        }
-    }
-    
+}
+
+// MARK: - Graphics and effects setup
+extension LoginViewController {
     private func okButtonChange(textFields: UITextField... ) {
         for textField in textFields {
             guard let text = textField.text, !text.isEmpty else { return }
             continue
         }
-            okButton.layer.shadowOpacity = 1
-            okButton.layer.shadowOffset = .zero
-            okButton.layer.shadowRadius = 20
-            okButton.layer.shadowColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
-            okButton.alpha = 1
+        okButton.layer.shadowOpacity = 1
+        okButton.layer.shadowOffset = .zero
+        okButton.layer.shadowRadius = 20
+        okButton.layer.shadowColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        okButton.alpha = 1
     }
-    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            textField?.text = ""
+    
+    func assignbackground(){
+        let background = UIImage(named: "stars")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
+    
+    func labelsSetup() {
+        for label in labels {
+            label.layer.shadowColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            label.layer.shadowOpacity = 1
+            label.layer.shadowRadius = 15
+            label.layer.shadowOffset = .zero
         }
+    }
+    
+    private func textFieldSetup(textField: UITextField) {
+        textField.layer.shadowOpacity = 1
+        textField.layer.shadowOffset = .zero
+        textField.layer.shadowRadius = 10
+        textField.layer.shadowColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+    }
+    
+    // MARK: - Allert setup
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
         alert.addAction(okAction)
         present(alert, animated: true)
     }
